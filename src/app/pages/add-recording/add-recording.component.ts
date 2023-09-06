8deadbef25e91284e9cb6ce7ef804b35f2d14bc5
Jessie Harvey <jessie.harvey120@gmail.com>
2023-09-07 08:03:37 +1000

    implemented basic recording functionality.

--- src/app/pages/add-recording/add-recording.component.ts
+++ src/app/pages/add-recording/add-recording.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./add-recording.component.css']
 })
 
-export class AddRecordingComponent implements OnInit{
+export class AddRecordingComponent implements OnInit {
 
     private mediaPromise!: Promise<MediaStream>;
 
@@ -22,8 +22,7 @@ export class AddRecordingComponent implements OnInit{
             console.log("No browser support");
             return
         }
-
-        this.mediaPromise = navigator.mediaDevices.getDisplayMedia({audio: true});
+        this.mediaPromise = navigator.mediaDevices.getUserMedia({audio: true});
         this.mediaPromise.then(stream => {
             navigator.mediaDevices.enumerateDevices()
                 .then(devices => {
@@ -40,10 +39,93 @@ export class AddRecordingComponent implements OnInit{
             this.killStream(stream)
         });
     }
-
     killStream(stream: MediaStream): void {
         stream.getTracks().forEach(track => {
             track.stop()
         })
+    }
+
+    start(): void {
+        if (this.stream) {
+            console.log("Existing Stream");
+            return
+        }
+
+        navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+            this.stream = stream;
+            this.mediaRecorder = new MediaRecorder(this.stream);
+            this.mediaRecorder.addEventListener("dataavailable", event => {
+                this.audioBlobs.push(event.data);
+                console.log("New blob recorded", event);
+            });
+
+            this.mediaRecorder.start();
+            console.log("Recording...");
+        })
+    }
+    stop(): void {
+        if (!this.mediaRecorder || !this.stream) return;
+
+        let mimeType = this.mediaRecorder.mimeType;
+
+        this.mediaRecorder.addEventListener("stop", () => {
+            console.log("Mime: ", mimeType);
+            this.audioBlob = new Blob(this.audioBlobs, {type: mimeType});
+            console.log("Size: ", this.audioBlob.size)
+        });
+
+        this.mediaRecorder.stop();
+        this.killStream(this.stream);
+        this.stream = null;
+        this.mediaRecorder = null;
+    }
+    play(): void {
+        if (!this.audioBlob) {
+            console.log("No audio blob.");
+            return
+        }
+
+        if (!this.audio) {
+            let reader = new FileReader();
+
+            reader.onload = (e) => {
+                let base64URL = e.target?.result?.toString();
+                if (!base64URL) return;
+
+                this.audio = new Audio();
+                this.audio.src = base64URL;
+                this.audio.load();
+                this.audio.play().then(() => {
+                    console.log("Played!");
+                });
+            };
+            reader.readAsDataURL(this.audioBlob);
+        } else {
+            this.audio.play().then(() => {
+                console.log("Played!");
+            })
+        }
+    }
+    download(): void {
+        if (!this.audioBlob) {
+            console.log("No audio blob.");
+            return
+        }
+
+        const a = document.createElement('a');
+        document.body.appendChild(a)
+        const url = window.URL.createObjectURL(this.audioBlob);
+        a.href = url;
+        const d = new Date();
+        a.download = d.toISOString()
+            .replace("T", "_")
+            .replace("Z", "UTC")
+            + "."
+            + this.audioBlob.type.split(";")[0].split("/")[1];
+        a.click();
+        setTimeout(() => {
+            window.URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+        }, 0)
     }
 }
